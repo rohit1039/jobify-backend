51e7201a7c9c02596b5761d4a7abe301e81805c3
Rohit Parida <81690863+rohit1039@users.noreply.github.com>
2023-02-16 15:23:08 +0530

    Delete client directory

--- client/src/pages/dashboard/index.js
+++
@@ -1,8 +0,0 @@
-import AddJob from "./AddJob";
-import Profile from "./Profile";
-import AllJobs from "./AllJobs";
-import AllUsers from "./AllUsers";
-import SharedLayout from "./SharedLayout";
-import Stats from "./Stats";
-
-export { AddJob, AllJobs, AllUsers, Profile, SharedLayout, Stats };
